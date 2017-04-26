b7934140d92a512f6286d08702f05897d806e97f
Franzé Jr <franzejr@gmail.com>
2017-04-26 14:43:34 -0300

    Add show for form

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -5,4 +5,9 @@ class Api::V1::FormsController < Api::V1::ApiController
     @forms = Form.all
     render json: @forms
   end
+
+  def show
+    @form = Form.find(params[:id])
+    render json: @form
+  end
 end
