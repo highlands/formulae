b78167ed1c23645765a089c285c044db5dff9d1f
Franzé Jr <franzejr@gmail.com>
2017-04-26 15:23:56 -0300

    Nest questions to forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :forms
+      resources :forms do
+        resources :questions
+      end
     end
   end
 end
