4b1c34bece5da46c7dbf8c1f9a5722e9d87af049
Franzé Jr <franzejr@gmail.com>
2017-04-27 00:14:18 -0300

    Remove applications and api keys. Not necessary.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :applications do
-        resources :api_keys
-      end
-
       resources :forms do
         resources :questions
       end
