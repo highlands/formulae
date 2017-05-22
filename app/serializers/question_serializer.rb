4fc6d95ce2c8fbcc311a890855627dabe02b627b
Josh Adams <josh.rubyist@gmail.com>
2017-05-22 12:54:42 -0500

    Add a bit more data in question dependency serialization

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -3,8 +3,15 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :key, :label, :content, :required, :order, :hidden, :question_type, :validate_as, :question_dependency
 
+  # Not calling question dependency serializer, not sure why
   def question_dependency
-    { choices: object.question_dependency.choices } if object.question_dependency
+    if object.question_dependency
+      {
+        display: object.question_dependency.display,
+        choices: object.question_dependency.choices,
+        and: object.question_dependency.and
+      }
+    end
   end
 
   # FIXME
