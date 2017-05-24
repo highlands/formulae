8e21e19c8743afe3c603208a5e8f4dcd5376a57b
Franzé Jr <franzejr@gmail.com>
2017-05-24 15:18:12 -0300

    Validates maximum_chosen for Questions Submissions

--- spec/acceptance/form_submissions_spec.rb
+++ spec/acceptance/form_submissions_spec.rb
@@ -31,7 +31,17 @@ resource 'Form Submissions' do
     parameter :form_id, scope: :form_submission
     parameter :question_submissions, scope: :form_submission
 
-    let(:question_submissions) { FactoryGirl.create_list(:question_submission, 3) }
+    before do
+      f = Form.find(form_id)
+      @q = f.questions.first
+      @q.choices << Choice.create(label: 'yes', maximum_chosen: 3)
+      @q.save!
+    end
+
+    let(:question_submissions) do
+      FactoryGirl.build_list(:question_submission, 1, question: @q, string: 'yes')
+    end
+
     let(:new_form_submission) { FactoryGirl.create(:form_submission) }
     let(:form_id) { new_form_submission.form.id }
 
@@ -40,6 +50,31 @@ resource 'Form Submissions' do
       expect(response.keys).to eq %w[id form]
       expect(response['form']['application_id']).to eq new_form_submission.form.application_id
       expect(status).to eq(201)
+    end
+  end
+
+  post '/api/v1/form_submissions' do
+    parameter :form_id, scope: :form_submission
+    parameter :question_submissions, scope: :form_submission
+
+    before do
+      f = Form.find(form_id)
+      @q = f.questions.first
+      @q.choices << Choice.create(label: 'yes', maximum_chosen: 1)
+      @q.save!
+    end
+
+    let(:question_submissions) do
+      FactoryGirl.build_list(:question_submission, 3, question: @q, string: 'yes')
+    end
+    let(:new_form_submission) { FactoryGirl.create(:form_submission) }
+    let(:form_id) { new_form_submission.form.id }
+
+    example_request 'Form submission failing' do
+      response = JSON.parse(response_body)
+      expect(response.keys).to eq ['error']
+      expect(response.values).to eq ['Validation failed: Maxchosen This choice has been reached up the max chosen']
+      expect(status).to eq(500)
     end
   end
 
--- spec/models/question_submission_spec.rb
+++ spec/models/question_submission_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe QuestionSubmission, type: :model do
     it { is_expected.to belong_to(:form_submission) }
     it { is_expected.to belong_to(:question) }
   end
+
+  describe 'validate max chosen' do
+    let(:question) do
+      q = FactoryGirl.create(:question)
+      q.choices << FactoryGirl.create(:choice, label: 'YES', maximum_chosen: 1)
+      q
+    end
+
+    it 'returns a validation error if maximum chosen is reached up' do
+      expect do
+        QuestionSubmission.create(question: question, string: 'YES')
+      end.not_to raise_error
+
+      expect do
+        QuestionSubmission.create!(question: question, string: 'YES')
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
