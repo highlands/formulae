2f8f1d1a02098586690c208cc69e96583bea5544
Franzé Jr <franzejr@gmail.com>
2017-06-21 18:25:28 -0300

    Add ability to edit nested attributes manually

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -22,6 +22,9 @@ class Api::V1::FormsController < Api::V1::ApiController
 
   def create
     ActiveRecord::Base.transaction do
+      @form = Form.find(params[:form][:id])
+      return edit_form if @form
+
       @form = Form.new(form_params)
       sections = params[:form][:sections]
       if @form.save!
@@ -31,6 +34,21 @@ class Api::V1::FormsController < Api::V1::ApiController
         end
         render json: @form, status: :created
       else
+        render json: @form.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private def edit_form
+    ActiveRecord::Base.transaction do
+      begin
+        sections = params[:form][:sections]
+        sections.each do |section_params|
+          edit_section_for(section_params)
+          edit_question_for(section_params)
+        end
+        render json: @form, status: :no_content
+      rescue
         render json: @form.errors, status: :unprocessable_entity
       end
     end
@@ -63,6 +81,19 @@ class Api::V1::FormsController < Api::V1::ApiController
     )
   end
 
+  def edit_section_for(section_params)
+    section_id = section_params[:id]
+    section = Section.find_by(id: section_id)
+    section = Section.create unless section
+
+    section.update_attributes!(
+      name: section_params[:name],
+      order: section_params[:order],
+      content: section_params[:content],
+      form: @form
+    )
+  end
+
   def create_question_for(section_params, section)
     section_params[:questions].each do |q|
       Question.create!(
@@ -77,5 +108,31 @@ class Api::V1::FormsController < Api::V1::ApiController
         section: section
       )
     end
+  end
+
+  def edit_question_for(section_params)
+    section_id = section_params[:id]
+    section = Section.find(section_id)
+    questions_params = section_params[:questions]
+
+    old_questions = Question.where(section_id: section.id).map(&:id)
+    added_questions = questions_params.map do |q_params|
+      q = Question.find_by(id: q_params[:id])
+      q = Question.create unless q
+      q.update_attributes!(
+        key: q_params[:key],
+        label: q_params[:label],
+        content: q_params[:content],
+        order: q_params[:order],
+        placeholder: q_params[:placeholder],
+        validate_as: q_params[:validate_as],
+        required: q_params[:required],
+        question_type: q_params[:question_type],
+        section: section
+      )
+      q.id
+    end
+    to_be_deleted = old_questions - added_questions
+    Question.destroy(to_be_deleted)
   end
 end
