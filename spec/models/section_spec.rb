a8fdd95806531f8fdf24dba33b3ae07422cb550d
Franzé Jr <franzejr@gmail.com>
2017-06-22 11:11:10 -0300

    Add comments for make params

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe Section, type: :model do
 
   describe 'relations' do
     it { is_expected.to belong_to(:form) }
-    it { is_expected.to have_one(:question) }
+    it { is_expected.to have_many(:questions) }
   end
 end
