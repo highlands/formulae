ffc518c4caa9db5a6f7ab5e0251b98512d10d44c
Jacob Roberts <jacob@churchofthehighlands.com>
2020-01-09 13:04:59 -0600

    [HV-4] Updates deploy scripts, ruby versions

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,2 @@
-set :application, 'formulae'
-ip = "formulae"
-server ip, user: 'deployer', roles: %w{web db app}
+set :branch, 'production'
+server 'formulae', user: 'deployer', roles: %w{web db app}
