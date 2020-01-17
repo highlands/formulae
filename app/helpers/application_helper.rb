a372e84a4ea0286034061a00556498a517777aa8
Jacob Roberts <jacob@churchofthehighlands.com>
2020-01-17 10:16:40 -0600

    [HV-4] Adds unique_dom_id helper to generate unique ids for all elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
+  def unique_dom_id(f)
+    parent = ''
+    if f.options[:parent_builder].present?
+      parent = "#{unique_dom_id(f.options[:parent_builder])}_"
+    end
+    return "#{parent}#{dom_class(f.object)}_#{f.index}"
+  end
 end
